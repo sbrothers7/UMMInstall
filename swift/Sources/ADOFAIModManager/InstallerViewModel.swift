2e6b7444cf3e2f07d7321c97c347c6f4e8d746d8
sbrothers7 <92313509+sbrothers7@users.noreply.github.com>
2026-06-02 07:37:47 +0900

    xperfect update

--- swift/Sources/ADOFAIModManager/InstallerViewModel.swift
+++ swift/Sources/ADOFAIModManager/InstallerViewModel.swift
@@ -126,7 +126,7 @@ final class InstallerViewModel: ObservableObject {
             return ModRegistry.all.filter { !$0.v3Only }
         }
         let v3Excluded: Set<String> = [
-            "AdofaiTweaks", "TUFHelper", "XPerfect", "DesyncFix", "TogetherBootstrap", "PACL2"
+            "AdofaiTweaks", "TUFHelper", "DesyncFix", "TogetherBootstrap", "PACL2"
         ]
         return ModRegistry.all.filter { !v3Excluded.contains($0.id) }
     }
